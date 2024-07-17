e3dbbebdc45858246ae56b5759c653bec0a4ab00
Marius Zilinskas <mariuzmail@gmail.com>
2024-07-17 02:02:03 +0100

    init repo

---
+++ src/main/java/com/mariuszilinskas/vsp/searchservice/SearchServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mariuszilinskas.vsp.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariuszilinskas/vsp/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariuszilinskas.vsp.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
